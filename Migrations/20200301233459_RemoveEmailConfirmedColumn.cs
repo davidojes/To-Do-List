238d956313be4d20624de8ef2de1b58649a2fca9
David Ojesekhoba <ojesekho@sheridancollege.ca>
2020-03-01 18:38:41 -0500

    Removed unused fields from the database

---
+++ Migrations/20200301233459_RemoveEmailConfirmedColumn.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+  public partial class RemoveEmailConfirmedColumn : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(name: "EmailConfirmed", table: "AspNetUsers");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+  }
+}
